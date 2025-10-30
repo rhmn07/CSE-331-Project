f2ff61909ac0100eb6d9e422c4b46bd245aea897
kyler <kylerhuy@buffalo.edu>
2025-10-30 17:29:25 -0400

    OptimalPath finder

--- MPJava2/src/ub/cse/algo/Solution.java
+++ MPJava2/src/ub/cse/algo/Solution.java
@@ -1,9 +1,7 @@
 package ub.cse.algo;
 
 import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.*;
 
 public class Solution {
 
@@ -25,7 +23,7 @@ public class Solution {
     }
 
     /**
-     * Method that returns the calculated 
+     * Method that returns the calculated
      * SolutionObject as found by your algorithm
      *
      * @return SolutionObject containing the paths, priorities and bandwidths
@@ -44,8 +42,6 @@ public class Solution {
         sol.paths = optimalPaths(this.clients);
 
 
-
-
         //System.out.println(sol.priorities);
         //System.out.println(sol.bandwidths);
         //System.out.println(sol.paths);
@@ -53,9 +49,6 @@ public class Solution {
     }
 
     public HashMap<Integer, ArrayList<Integer>> optimalPaths(ArrayList<Client> clients) {
-        HashMap<Integer, ArrayList<Integer>> pathList = new HashMap<>();
-
-
         int maxAlpha = 0;
         for (Client client : clients) {
             if (!Float.isInfinite(client.alpha)) {
@@ -70,13 +63,14 @@ public class Solution {
             int i = 0;
             if (Float.isInfinite(client.alpha)) {
                 i = maxAlpha;
-            }
-            else{
-                i = (int)  client.alpha;
+            } else {
+                i = (int) client.alpha;
             }
             alphaSort.get(i).add(client);
         }
 
+        System.out.println(maxAlpha);
+        System.out.println("");
         for (int i = 0; i < alphaSort.size(); i++) {
             alphaSort.set(i, sortClientsByAlpha(alphaSort.get(i)));
         }
@@ -84,7 +78,7 @@ public class Solution {
 
         //PERFORM A BFS FOR PATH TO EACH NODE, KEEP TRACK OF BANDWIDTH OF EACH NODE, CANNOT USE IF MAX B > USED B
 
-        return pathList;
+        return makePaths(alphaSort);
     }
 
     public ArrayList<Client> sortClientsByAlpha(ArrayList<Client> clients) {
@@ -100,5 +94,58 @@ public class Solution {
             clients.remove(max);
         }
         return sortedClients;
+    }
+
+    public HashMap<Integer, ArrayList<Integer>> makePaths(ArrayList<ArrayList<Client>> sortedClients) {
+        HashMap<Integer, ArrayList<Integer>> pathList = new HashMap<>();
+        HashMap<Integer, Integer> currBans = new HashMap<>();
+        for (int i = 0; i < this.graph.size(); i++) {
+            currBans.put(i, 0);
+        }
+
+        //System.out.println(currBans);
+
+
+        //REWORKED TRAVERSALS BFS FOR ONLY ONE CLIENT & ACCOUNTING FOR BANDWIDTHS
+        for (int i = 0; i < sortedClients.size(); i++) { //FOR EACH ALPHA VALUE
+            for (int j = 0; j < sortedClients.get(i).size(); j++) { //FOR EACH CLIENT, STARTING WITH HIGHEST PAYING
+                Client client = sortedClients.get(i).get(j);
+
+                Queue<Integer> searchQueue = new LinkedList<>();
+                HashMap<Integer, Integer> priors = new HashMap<>();
+                searchQueue.add(graph.contentProvider);
+                priors.put(graph.contentProvider, -1);
+                boolean found = false;
+
+                while (!searchQueue.isEmpty() && !found) {
+                    int node = searchQueue.poll();
+                    for (int neighbor : graph.get(node)) {
+                        if (!priors.containsKey(neighbor) && currBans.get(neighbor) >= this.bandwidths.get(neighbor)) {
+                            priors.put(neighbor, node);
+                            searchQueue.add(neighbor);
+                        }
+                        if (neighbor == client.id) {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (found) {
+                    ArrayList<Integer> clientPath = new ArrayList<>();
+                    int current = client.id;
+                    while (current != -1) {
+                        clientPath.add(0, current);
+                        current = priors.get(current);
+                    }
+                    for (int c : clientPath) {
+                        currBans.replace(c, currBans.get(c) + 1);
+                    }
+                    pathList.put(client.id, clientPath);
+                }
+                //OTHERWISE, WE DITCH THIS CLIENT
+            }
+        }
+        return pathList;
     }
 }
