1af5656e7974ee8908ea0b0cb9a405320cba8504
kyler <kylerhuy@buffalo.edu>
2025-10-31 14:54:14 -0400

    New & Polished Solution

--- MPJava2/src/ub/cse/algo/Solution.java
+++ MPJava2/src/ub/cse/algo/Solution.java
@@ -33,109 +33,56 @@ public class Solution {
         /* TODO: Your solution goes here */
         sol.bandwidths = this.bandwidths;
         sol.priorities = new HashMap<Integer, Integer>(this.graph.size());
-        System.out.println(bandwidths);
-        System.out.println(clients);
         for (int i = 0; i < this.clients.size(); i++) {
             sol.priorities.put(i, this.clients.get(i).priority);
         }
         HashMap<Integer, ArrayList<Integer>> shortestPath = Traversals.bfsPaths(this.graph, this.clients);
 
-        //System.out.println(this.clients);
         sol.paths = optimalPaths(this.clients);
         System.out.println(sol.paths);
         return sol;
     }
 
     public HashMap<Integer, ArrayList<Integer>> optimalPaths(ArrayList<Client> clients) {
-        //START OF MAHIR'S SECTION -------------------------
-        int maxAlpha = 0;
-        for (Client client : clients) {
-            if (!Float.isInfinite(client.alpha)) {
-                maxAlpha = Math.max(maxAlpha, (int) client.alpha);
+        //-------------------------------
+        ArrayList<Client> copyClient = new ArrayList<>(clients);
+        HashMap<Integer, Integer> optimalPaths = Traversals.bfs(this.graph, this.clients);
+        ArrayList<Client> sorted = new ArrayList<>();
+        while (!copyClient.isEmpty()) {
+            double maxVal = -1;
+            Client maxClient = null;
+            for (Client client : copyClient) {
+                double currentVal = (client.payment*1000) + (client.alpha * 1000000) + optimalPaths.get(client.id);
+                if (maxVal == -1){
+                    maxClient = client;
+                    maxVal = currentVal;
+                }
+                else{
+                    if (maxVal < currentVal){
+                        maxClient = client;
+                        maxVal = currentVal;
+                    }
+                }
             }
+            sorted.add(maxClient);
+            copyClient.remove(maxClient);
         }
-        ArrayList<ArrayList<Client>> alphaSort = new ArrayList<>();
-        for (int i = 0; i <= maxAlpha; i++) {
-            alphaSort.add(new ArrayList<>());
-        }
-        for (Client client : clients) {
-            int i = 0;
-            if (Float.isInfinite(client.alpha)) {
-                i = maxAlpha;
-            } else {
-                i = (int) client.alpha;
-            }
-            alphaSort.get(i).add(client);
-        }
-        // END OF MAHIR'S SECTION --------------------
-        HashMap<Integer, Integer> optimality = Traversals.bfs(this.graph, this.clients);
-        for (int i = 0; i < alphaSort.size(); i++) {
-            if (!alphaSort.get(i).isEmpty()) {
-                alphaSort.set(i, sortClientsInAlpha(alphaSort.get(i), optimality));
-            }
-        }
+        //----------------------------------------------
         //PERFORM A BFS FOR PATH TO EACH NODE, KEEP TRACK OF BANDWIDTH OF EACH NODE, CANNOT USE IF MAX B > USED B
-        return makePaths(alphaSort);
+        return makePaths(sorted);
     }
 
-    public ArrayList<Client> sortClientsInAlpha(ArrayList<Client> clients, HashMap<Integer, Integer> optimality) {
-        ArrayList<Client> sortedClients = new ArrayList<>();
-        while (clients.size() > 0){
-            int min = -1;
-            Client minCli = null;
-            for (Client client : clients) {
-                if (min == -1) {
-                    min = client.id;
-                    minCli = client;
-                }
-                if (optimality.get(min) <= optimality.get(client.id)){
-                    if (optimality.get(min) < optimality.get(client.id)) {
-                        min = client.id;
-                        minCli = client;
-                    }
-                    else{
-                        if (minCli.payment < client.payment) {
-                            minCli = client;
-                            min = client.id;
-                        }
-
-                    }
-                }
-            }
-            if (min != -1) {
-                sortedClients.add(minCli);
-                clients.remove(minCli);
-            }
-        }
-//
-//       while (clients.size() > 0) {
-//            Client max = clients.getFirst();
-//            for (int i = 1; i < clients.size() - 1; i++) {
-//                if (max.payment < clients.get(i).payment) {
-//                    max = clients.get(i);
-//                }
-//            }
-//            sortedClients.add(max);
-//            clients.remove(max);
-//        }
-        return sortedClients;
-    }
-
-    public HashMap<Integer, ArrayList<Integer>> makePaths(ArrayList<ArrayList<Client>> sortedClients) {
+    public HashMap<Integer, ArrayList<Integer>> makePaths(ArrayList<Client> sortedClients) {
         HashMap<Integer, ArrayList<Integer>> pathList = new HashMap<>();
         HashMap<Integer, Integer> currBans = new HashMap<>();
+        //IDK another way to get all the keys, I don't know Java very well - Kyler
         Set<Integer> keys = this.graph.keySet();
         for (Integer key : keys) {
             currBans.put(key, 0);
         }
-
-        //System.out.println(currBans);
-
-
         //REWORKED TRAVERSALS BFS FOR ONLY ONE CLIENT & ACCOUNTING FOR BANDWIDTHS
-        for (int i = 0; i < sortedClients.size(); i++) { //FOR EACH ALPHA VALUE
-            for (int j = 0; j < sortedClients.get(i).size(); j++) { //FOR EACH CLIENT, STARTING WITH HIGHEST PAYING
-                Client client = sortedClients.get(i).get(j);
+        for (int i = 0; i < sortedClients.size(); i++) { //FOR EACH ALPHA VALUE//FOR EACH CLIENT, STARTING WITH HIGHEST PAYING
+                Client client = sortedClients.get(i);
 
                 Queue<Integer> searchQueue = new LinkedList<>();
                 HashMap<Integer, Integer> priors = new HashMap<>();
@@ -171,7 +118,6 @@ public class Solution {
                 }
                 pathList.put(client.id, clientPath);
             }
-        }
         return pathList;
     }
 }
